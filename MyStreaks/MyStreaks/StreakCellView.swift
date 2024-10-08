884260a0fc88166ff0d66931dba17a53c947aee8
Владислав <vlad2004ki@gmail.com>
2024-10-08 17:09:44 +0300

    progress bar

--- MyStreaks/MyStreaks/StreakCellView.swift
+++ MyStreaks/MyStreaks/StreakCellView.swift
@@ -8,7 +8,21 @@ struct StreakCellView: View {
             Image(systemName: "car").padding(.trailing, 10)
             VStack(alignment: .leading) {
                 Text(streak.name).font(.title3)
-                Text(streak.streakDescription ?? "").font(.caption).foregroundStyle(Color.gray)
+                ZStack(alignment: .leading) {
+                    // Background rectangle
+                    Rectangle()
+                        .frame(width: .infinity, height: 10)
+                        .opacity(0.3)
+                        .foregroundColor(.gray)
+                    
+                    // Progress rectangle
+                    Rectangle()
+                        .frame(
+                            width: CGFloat(streak.moneyCount) / CGFloat(streak.goal) * UIScreen.main.bounds.width * 0.7, // Adjust the multiplier as needed
+                            height: 10
+                        )
+                        .foregroundColor(.blue)
+                }
             }.bold()
             Spacer()
             
@@ -18,5 +32,5 @@ struct StreakCellView: View {
 }
 
 #Preview {
-    StreakCellView(streak: Streak(name: "My Streak", goal: 1000, moneyCount: 19.0, streakDescription: "My Dream"))
+    StreakCellView(streak: Streak(name: "My Streak", goal: 1000, moneyCount: 121.0, streakDescription: "My Dream"))
 }
