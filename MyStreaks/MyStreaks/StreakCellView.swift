817860f194d0c5b7cff19bea46dffdaa6553995c
Владислав <vlad2004ki@gmail.com>
2024-10-08 19:53:19 +0300

    small progress bar upd

--- MyStreaks/MyStreaks/StreakCellView.swift
+++ MyStreaks/MyStreaks/StreakCellView.swift
@@ -4,33 +4,34 @@ struct StreakCellView: View {
     let streak: Streak
     
     var body: some View {
-        HStack {
-            Image(systemName: "car").padding(.trailing, 10)
-            VStack(alignment: .leading) {
-                Text(streak.name).font(.title3)
-                ZStack(alignment: .leading) {
-                    // Background rectangle
-                    Rectangle()
-                        .frame(width: .infinity, height: 10)
-                        .opacity(0.3)
-                        .foregroundColor(.gray)
-                    
-                    // Progress rectangle
-                    Rectangle()
-                        .frame(
-                            width: CGFloat(streak.moneyCount) / CGFloat(streak.goal) * UIScreen.main.bounds.width * 0.7, // Adjust the multiplier as needed
-                            height: 10
-                        )
-                        .foregroundColor(.blue)
-                }
-            }.bold()
-            Spacer()
-            
-            Text("\(streak.goal) $").bold().font(.title2)
+        VStack {
+            HStack {
+                Image(systemName: "car").padding(.trailing, 10)
+                VStack(alignment: .leading) {
+                    Text(streak.name).font(.title3)
+                    Text(streak.streakDescription ?? "").font(.caption).foregroundStyle(Color.gray)
+                }.bold()
+                Spacer()
+                
+                Text("\(streak.goal) $").bold().font(.title3).padding(.leading, 10)
+            }
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: .infinity, height: 15)
+                    .opacity(0.3)
+                    .foregroundColor(.gray)
+                
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(
+                        width: CGFloat(streak.moneyCount) / CGFloat(streak.goal) * UIScreen.main.bounds.width * 0.7,
+                        height: 15
+                    )
+                    .foregroundColor(.blue)
+            }.padding(.horizontal)
         }
     }
 }
 
 #Preview {
-    StreakCellView(streak: Streak(name: "My Streak", goal: 1000, moneyCount: 121.0, streakDescription: "My Dream"))
+    StreakCellView(streak: Streak(name: "My Streak", goal: 1000, moneyCount: 421.0, streakDescription: "My Dream"))
 }
