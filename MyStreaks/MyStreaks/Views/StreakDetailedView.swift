b16ac283a413229d19b8d1623fd56b82c28a32a1
Владислав <vlad2004ki@gmail.com>
2024-10-18 11:16:30 +0300

    streakdetails

--- MyStreaks/MyStreaks/Views/StreakDetailedView.swift
+++ MyStreaks/MyStreaks/Views/StreakDetailedView.swift
@@ -1,12 +1,96 @@
 import SwiftUI
+import PhotosUI
 import SwiftData
 
 struct StreakDetailedView: View {
+    @Environment(\.modelContext) private var modelContext: ModelContext
     @State var streak: Streak
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var name: String
+    @State private var image: Data?
+    @State private var goal: String
+    @State private var streakDescription: String
+    
+    init(streak: Streak) {
+        _streak = State(initialValue: streak)
+        _name = State(initialValue: streak.name)
+        _image = State(initialValue: streak.image)
+        _goal = State(initialValue: String(streak.goal))
+        _streakDescription = State(initialValue: streak.streakDescription ?? "")
+    }
     
     var body: some View {
-        VStack {
-            
+        NavigationStack{
+            VStack {
+                if let data = image, let uiImage = UIImage(data: data) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height * 0.3)
+                        .padding()
+                }
+                
+                
+                Form {
+                    Section("Main information"){
+                        TextField("Streak Name", text: $name)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled(true)
+                        TextField("Streak Goal", text: $goal)
+                            .textContentType(.oneTimeCode)
+                            .keyboardType(.numberPad)
+                            .autocapitalization(.none)
+                            .autocorrectionDisabled(true)
+                    }
+                    Section(){
+                        PhotosPicker(
+                            selection: $selectedPhoto,
+                            matching: .any(of: [.images, .screenshots]),
+                            photoLibrary: .shared()
+                        ) {
+                            Text("Upload Image")
+                        }
+                    }
+                    Section("Description"){
+                        TextEditor(text: $streakDescription)
+                            .frame(height: 150)
+                            .onReceive(streakDescription.publisher.collect()) {
+                                self.streakDescription = String($0.prefix(150))
+                            }
+                    }
+                }
+            }
+            .toolbar {
+                Button("Update") {
+                    
+                }.disabled(name.isEmpty && goal.isEmpty)
+            }
+            .navigationTitle("Streak details")
+        }
+        .onChange(of: selectedPhoto) { oldPhoto, newPhoto in
+            Task {
+                if newPhoto != nil {
+                    let imageData = try await selectedPhoto?.loadTransferable(type: Data.self)
+                       if let imageData = imageData {
+                           self.image = imageData
+                       }
+                    } else {
+                        print("Failed to load image")
+                    }
+                }
+            }
+        }
+    private func updateStreak(){
+        streak.name = name
+        streak.goal = Int(goal)!
+        streak.streakDescription = streakDescription
+        streak.image = image
+        
+        do {
+            try modelContext.save()
+        } catch {
+            _ = Alert(title: Text("Error"), message: Text(error.localizedDescription))
         }
     }
 }
